df0f888c15e1d262af40ad20bdd46a1c87f073ef
Sergei Kovalev <summerjob@ngs.ru>
2022-10-13 17:45:20 +0300

    SQL_REQUEST added

--- src/main/java/ru/ngs/summerjob/city/dao/PersonCheckDao.java
+++ src/main/java/ru/ngs/summerjob/city/dao/PersonCheckDao.java
@@ -10,7 +10,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class PersonCheckDao {
-    private static final String SQL_REQUEST = "";
+    private static final String SQL_REQUEST = """
+            SELECT temporal FROM cr_address_person ap
+            INNER JOIN cr_person p on p.person_id = ap.person_id
+            INNER JOIN cr_address a on a.address_id = ap.address_id
+            WHERE\s
+            UPPER(p.sur_name) = UPPER(?)\s
+            and UPPER(p.given_name) = UPPER(?)\s
+            and UPPER(p.patronymic_name) = UPPER(?)
+            and p.date_of_birth = ?
+            and a.street_code = ? and UPPER(a.building) = UPPER(?)\s
+            and UPPER(a.extension) = UPPER(?) and UPPER(a.apartment) = UPPER(?)""";
 
     public PersonResponse checkPerson(PersonRequest request) throws PersonCheckException {
         PersonResponse response = new PersonResponse();
