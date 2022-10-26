5ab136036fe89577aa0c4f7f247b992756c97dd6
Sergei Kovalev <summerjob@ngs.ru>
2022-10-26 17:30:00 +0300

    Git + refactored http request

---
+++ src/test/java/ru/ngs/summerjob/net/SimpleSocket.java
@@ -0,0 +1,35 @@
+package ru.ngs.summerjob.net;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class SimpleSocket {
+
+    @Test
+    public void simpleSocket() throws IOException {
+        Socket socket = new Socket("localhost", 8080);
+
+        InputStream is = socket.getInputStream();
+        OutputStream os = socket.getOutputStream();
+
+        //String command = "GET /sitemap.xml HTTP/1.1\r\nHost:java-course.ru\r\n\r\n";
+        String command = """
+                GET / HTTP/1.1
+                Host:java-course.ru
+                
+                """;
+        os.write(command.getBytes());
+        os.flush();
+
+        int c = 0;
+        while ((c = is.read()) != -1) {
+            System.out.print((char) c);
+        }
+
+        socket.close();
+    }
+}
