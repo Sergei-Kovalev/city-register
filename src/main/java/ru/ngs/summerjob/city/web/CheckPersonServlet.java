5b2e4248bb90f3b7dfbf3fbd0fcd00f19785a9d8
Sergei Kovalev <summerjob@ngs.ru>
2022-10-27 17:07:09 +0300

    migrate from TomCat 9 to TomCat 10 (javax-api to jakarta-api)

--- src/main/java/ru/ngs/summerjob/city/web/CheckPersonServlet.java
+++ src/main/java/ru/ngs/summerjob/city/web/CheckPersonServlet.java
@@ -1,17 +1,17 @@
 package ru.ngs.summerjob.city.web;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet("/checkPerson")
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "CheckPersonServlet", urlPatterns = {"/checkPerson"})
 public class CheckPersonServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.getWriter().println("Get check person");
+        resp.getWriter().println("Hi");
     }
 }
